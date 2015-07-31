57dad62be1b96602d62798f0b1c48ec4ee00d580
Anton Leontiev <aleontiev@elvees.com>
2015-07-31 13:19:01 +0300

    Add Mcom-02 configuration files
    Change-Id: If7184f5b84ed9b036cad605b4ead751226e1592f

---
+++ r3p0-04rel0/driver/src/devicedrv/mali/arch-mcom02-m300/config.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2015-2018 RnD Center "ELVEES", JSC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef __ARCH_CONFIG_H__
+#define __ARCH_CONFIG_H__
+
+#define BASE_ADDR 0x37210000
+
+/* Configuration for the Mcom-02 platform */
+
+static _mali_osk_resource_t arch_configuration [] =
+{
+	{
+		.type = MALI300GP,
+		.description = "Mali-300 GP",
+		.base = BASE_ADDR | 0x0000,
+		.irq = -1,
+		.mmu_id = 1
+	},
+	{
+		.type = MALI300PP,
+		.description = "Mali-300 PP",
+		.base = BASE_ADDR | 0x8000,
+		.irq = -1,
+		.mmu_id = 2
+	},
+	{
+		.type = MMU,
+		.base = BASE_ADDR | 0x3000,
+		.irq = -1,
+		.description = "Mali-300 MMU for GP",
+		.mmu_id = 1
+	},
+	{
+		.type = MMU,
+		.base = BASE_ADDR | 0x4000,
+		.irq = -1,
+		.description = "Mali-300 MMU for PP",
+		.mmu_id = 2
+	},
+	{
+		.type = OS_MEMORY,
+		.description = "OS Memory",
+		.alloc_order = 10, /* Lowest preference for this memory */
+		.size = 128 * 1024 * 1024,
+		.flags = _MALI_CPU_WRITEABLE | _MALI_CPU_READABLE | _MALI_PP_READABLE | _MALI_PP_WRITEABLE |_MALI_GP_READABLE | _MALI_GP_WRITEABLE
+	},
+	{
+		.type = MEM_VALIDATION,
+		.description = "Framebuffer",
+		.base = 0x7f000000,
+		.size = 0x01000000,
+		.flags = _MALI_CPU_WRITEABLE | _MALI_CPU_READABLE | _MALI_PP_WRITEABLE | _MALI_PP_READABLE
+	},
+	{
+		.type = MALI300L2,
+		.base = BASE_ADDR | 0x1000,
+		.description = "Mali-300 L2 cache"
+	},
+};
+
+#endif /* __ARCH_CONFIG_H__ */
---
+++ r3p0-04rel0/driver/src/devicedrv/ump/arch-mcom02-m300/config.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2015-2018 RnD Center "ELVEES", JSC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef __ARCH_CONFIG_H__
+#define __ARCH_CONFIG_H__
+
+#ifndef USING_MEMORY
+#define USING_MEMORY	1
+#endif
+
+#define ARCH_UMP_BACKEND_DEFAULT		USING_MEMORY
+#if (USING_MEMORY == 0) /* Dedicated Memory */
+	#define ARCH_UMP_MEMORY_ADDRESS_DEFAULT		0x78000000
+#else
+	#define ARCH_UMP_MEMORY_ADDRESS_DEFAULT		0
+#endif
+#define ARCH_UMP_MEMORY_SIZE_DEFAULT 256UL * 1024UL * 1024UL
+
+#endif /* __ARCH_CONFIG_H__ */
