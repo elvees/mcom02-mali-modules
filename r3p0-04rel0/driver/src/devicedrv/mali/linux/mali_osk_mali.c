172598e8c3b88b39faa99eae5a9fbbe2d1c602eb
Anton Leontiev <aleontiev@elvees.com>
2016-09-23 12:57:07 +0300

    Add parameter to specify system memory size for Mali driver
    Change-Id: I102b3e118e387a0d44f76018053fe03598cadb7b

--- r3p0-04rel0/driver/src/devicedrv/mali/linux/mali_osk_mali.c
+++ r3p0-04rel0/driver/src/devicedrv/mali/linux/mali_osk_mali.c
@@ -13,12 +13,39 @@
  * Implementation of the OS abstraction layer which is specific for the Mali kernel device driver
  */
 #include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/stat.h>
 #include <asm/uaccess.h>
 
 #include "mali_kernel_common.h" /* MALI_xxx macros */
 #include "mali_osk.h"           /* kernel side OS functions */
 #include "mali_uk_types.h"
 #include "arch/config.h"        /* contains the configuration of the arch we are compiling for */
+
+static int mem_set(const char *val, const struct kernel_param *kp)
+{
+	unsigned long long m = memparse(val, NULL);
+
+	if (m < SZ_16M || m > SZ_2G)
+		return -EINVAL;
+
+	*(u32 *)kp->arg = m;
+
+	return 0;
+}
+
+static int mem_get(char *buffer, const struct kernel_param *kp)
+{
+	return scnprintf(buffer, PAGE_SIZE, "%u", *(u32 *)kp->arg);
+}
+
+static const struct kernel_param_ops param_mem_ops = {
+	.set = mem_set,
+	.get = mem_get
+};
+
+module_param_cb(mem, &param_mem_ops, &arch_configuration[4].size, S_IRUGO);
+MODULE_PARM_DESC(mem, "Mali system memory size");
 
 _mali_osk_errcode_t _mali_osk_resources_init( _mali_osk_resource_t **arch_config, u32 *num_resources )
 {
